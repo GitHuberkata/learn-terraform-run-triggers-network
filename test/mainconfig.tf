b6b75f93ab50c8cf591e31817e811d58db838288
GitHuberkata <petyagavrilova86@gmail.com>
2023-03-07 12:36:58 +0200

    Create mainconfig.tf

---
+++ test/mainconfig.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "random" {
+
+}
+
+resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name on each apply
+
+  }
+
+}
+
+
+output "value" {
+  value = "MyNewPetTypeIs-${random_pet.server.id}"
+
+}
